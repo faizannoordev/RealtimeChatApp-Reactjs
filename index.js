91ab36c3024e82ba190e099c8828ab06203bd590
Faizan <faizannoor465@gmail.com>
2023-07-06 17:58:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDom from 'react-dom/client'
 import App from './src/App';
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsQSqGE1JS3_AgXBAmjkJUTQuN05GY_1M",
-  authDomain: "chat-app-a3462.firebaseapp.com",
-  databaseURL: "https://chat-app-a3462-default-rtdb.firebaseio.com",
-  projectId: "chat-app-a3462",
-  storageBucket: "chat-app-a3462.appspot.com",
-  messagingSenderId: "525283744964",
-  appId: "1:525283744964:web:78c7b87b1c6d9335917d91",
-  measurementId: "G-5Y53MJW7TV"
+    apiKey: "AIzaSyD3i9NWXKfcnU382QlmQTS5oIw_WIHVckw",
+    authDomain: "chatroom-9dfe0.firebaseapp.com",
+    projectId: "chatroom-9dfe0",
+    storageBucket: "chatroom-9dfe0.appspot.com",
+    messagingSenderId: "576563157577",
+    appId: "1:576563157577:web:f009dffa27196e95c23d98",
+    measurementId: "G-LH3YRFDQR2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />)
+const analytics = getAnalytics(app);
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(
+    <App />,
+)
